18f190e93455079f01da03c6627300af4a0b20a9
David Flores <davidfloresgarcia55516@gmail.com>
2025-03-03 16:48:36 -0500

    new folder structure & db start up

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config({path: "./.env"}); // Load environment variables
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+
+    } catch (error) {
+        console.error(`MongoDB Connection Error: ${error.message}`);
+        process.exit(1); // Stop the server if DB connection fails
+    }
+};
+
+module.exports = connectDB;
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+require("dotenv").config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public")); // Serve static files
+
+// Connect to Database
+connectDB();
+
+// API Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
