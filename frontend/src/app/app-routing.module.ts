3c9263ec0ae4e89ece295d89c23d0617cb7fe084
Sebastian Londono Escobar <slondonoescobar@ggc.edu>
2025-10-18 23:58:33 -0400

    Trying to modify the index with angular, created an about page that does not have access to dashboard without an account

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { IndexPageComponent } from './components/home/index-page.component';
+
+export const routes: Routes = [
+  { path: '', component: IndexPageComponent },
+];
