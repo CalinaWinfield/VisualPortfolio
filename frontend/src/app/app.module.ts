bebf6b612b1935de3d800141a199e73e9ef2dbb5
Jordan Laudun <jlaudun@ggc.edu>
2025-11-06 14:28:32 -0500

    Angular fixes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,20 +2,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './components/about/about.component';
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DocumentComponent } from './components/document/document.component';
+import { CoverLetterComponent } from './components/cover-letter/cover-letter.component';
+import { CreateItemComponent } from './components/create-item/create-item.component';
+import { GuideComponent } from './components/guide/guide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    DashboardComponent,
+    DocumentComponent,
+    CoverLetterComponent,
+    CreateItemComponent,
+    GuideComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
